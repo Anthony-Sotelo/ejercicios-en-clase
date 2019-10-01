3d0d257d47f65cf299f5e46de28c8c9a71e05c40
Anthony-Sotelo <55764721+Anthony-Sotelo@users.noreply.github.com>
2019-10-01 14:31:59 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+realizar un programa en C que determine el area de un circulo
+
+*******************************************************************************/
+#include <stdio.h>
+#define pi 3.1416
+
+void main()
+{
+    float radio,area=0;
+    printf("ingrese el radio");
+    scanf("%f",&radio);
+    area=pi*radio*radio;
+    printf("el area es %f",area);
+ 
+}
